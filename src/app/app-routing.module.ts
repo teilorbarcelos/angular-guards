d03d2b3a6ef6e3e856e061521bae5f1192802be4
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-24 17:27:23 -0300

    project clear, guard created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AccountComponent } from './shared/account/account.component'
+import { CanActiveGuard } from './shared/guards/can-active.guard'
+import { HomeComponent } from './shared/home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [CanActiveGuard],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
